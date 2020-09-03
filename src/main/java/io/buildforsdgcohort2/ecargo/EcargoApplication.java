319483728655bdcdb95ef838870043b6bda80c50
credoDeveloper <aguetec@gmail.com>
2020-09-03 14:51:30 +0000

    initial commit

---
+++ src/main/java/io/buildforsdgcohort2/ecargo/EcargoApplication.java
@@ -0,0 +1,13 @@
+package io.buildforsdgcohort2.ecargo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcargoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcargoApplication.class, args);
+	}
+
+}
